937546632e5d573488b5575516ebe64e51a39ef9
Bradley D. Willard <bradley.d.willard@gmail.com>
2016-06-08 10:56:40 -0700

    Removing

--- CS111C/src/org/bwillard/ccsf/course/cs111c/homework14/BinaryTreeInterface.java
+++
@@ -1,9 +0,0 @@
-package org.bwillard.ccsf.course.cs111c.homework14;
-
-public interface BinaryTreeInterface<T> extends TreeInterface<T>, TreeIteratorInterface<T> {
-	
-	public void setTree(T rootData);
-	
-	public void setTree(T rootData, BinaryTreeInterface<T> leftTree, BinaryTreeInterface<T> rightTree);
-
-}
